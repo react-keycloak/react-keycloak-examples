b91254b1f41627b642f9d5c0c05b846a0f8d7897
Øyvind Saltvik <oyvind.saltvik@gmail.com>
2020-09-14 19:29:01 +0200

    fix(razzle.app): get correct window env variables

--- examples/razzle-app/src/utils.js
+++ examples/razzle-app/src/utils.js
@@ -4,15 +4,9 @@ function getKeycloakConfig() {
   return typeof window !== 'undefined' && window.env !== 'undefined'
     ? {
         // client
-        url: window.env
-          ? window.env.KEYCLOAK_URL
-          : serverEnv('KEYCLOAK_URL', 'http://localhost:8080/auth'),
-        clientId: window.env
-          ? window.env.KEYCLOAK_CLIENT_ID
-          : serverEnv('KEYCLOAK_CLIENT_ID', 'react-test'),
-        realm: window.env
-          ? window.env.KEYCLOAK_REALM
-          : serverEnv('KEYCLOAK_REALM', 'Test'),
+        url: window.env.url,
+        clientId: window.env.clientId,
+        realm: window.env.realm
       }
     : {
         // server
